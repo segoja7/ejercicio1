c5217649e709a3cd980b26158d37e795ece61eb4
Javier Sepúlveda <javiersepulveda12@hotmail.com>
2023-11-03 20:23:28 +0000

    adding package validation

--- grupo7-deploy.sh
+++ grupo7-deploy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-#check user id
 if [ "$(whoami)" != "root" ]; then
   echo "Necesitas otro usuario"
   exit 1
 else
-  echo "El script se ejecutó con éxito."
-  exit 0  
+  echo "usuario validado: $(whoami)"
+  
+  paquetes="git apache2 php mariadb-server"
+  for i in $paquetes; do
+    if dpkg -l | grep -q "$i"; then
+      echo "el paquete: $i, ya esta instalado"
+    else
+      echo "el paquete: $i, no esta instalado"
+      echo "Instalando $i"
+      sudo apt update -y
+      sudo apt install $i -y      
+    fi
+  done
 fi
-
-
