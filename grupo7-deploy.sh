5181259a6ff1519bfd0dc95f06898a17340b154e
Javier Sepúlveda <javiersepulveda12@hotmail.com>
2023-11-03 16:03:22 -0500

    Update grupo7-deploy.sh

--- grupo7-deploy.sh
+++ grupo7-deploy.sh
@@ -6,7 +6,7 @@ if [ "$(whoami)" != "root" ]; then
 else
   echo "usuario validado: $(whoami)"
   
-  paquetes="git apache2 php mariadb-server"
+  paquetes="git apache2 php mariadb-server pwgen"
   for i in $paquetes; do
     if dpkg -l | grep -q "$i"; then
       echo "el paquete: $i, ya esta instalado"
@@ -14,7 +14,12 @@ else
       echo "el paquete: $i, no esta instalado"
       echo "Instalando $i"
       sudo apt update -y
-      sudo apt install $i -y      
+      sudo apt install $i -y
+      #Parametrizando mariadb.
+      password_mariadb=$(pwgen 25 1 --symbols)
+      mysql_secure_installation --host=::1 --port=3333 --password=$password_mariadb --user=testing
+      #testing
+      echo $password_mariadb
     fi
   done
 fi
