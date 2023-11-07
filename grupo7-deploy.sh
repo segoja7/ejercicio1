818d8cc792242a3e20210c6778e98407d14e544c
Javier Sepúlveda <javiersepulveda12@hotmail.com>
2023-11-07 18:32:00 -0500

    Update grupo7-deploy.sh

--- grupo7-deploy.sh
+++ grupo7-deploy.sh
@@ -5,27 +5,29 @@ if [ "$(whoami)" != "root" ]; then
   exit 1
 else
   echo "usuario validado: $(whoami)"
-  
-  paquetes="git apache2 php mariadb-server pwgen"
-  for i in $paquetes; do
-    if dpkg -l | grep -q "$i"; then
-      echo "el paquete: $i, ya esta instalado"
-    else
-      echo "el paquete: $i, no esta instalado"
-      echo "Instalando $i"
-      sudo apt update -y
-      sudo apt install $i -y
-      #Parametrizando mariadb.
-      password_mariadb=$(pwgen 25 1 --symbols)
-      echo "mysql-server mysql-server/root_password password $password_mariadb" | sudo debconf-set-selections
-      echo "mysql-server mysql-server/root_password_again password $password_mariadb" | sudo debconf-set-selections 
-      #testing
-      export DEBIAN_FRONTEND="noninteractive"
-      echo $password_mariadb 
-      mysql_secure_installation
-    fi
-  done
 fi
-
-password_mariadb=$(pwgen 25 1 --symbols)
-echo $password_mariadb
+#################################
+password_mariadb=""
+paquetes="git apache2 php pwgen mariadb-server"
+for i in $paquetes; do
+        if dpkg -l | grep -q "$i"; then
+                echo "el paquete: $i, ya esta instalado"
+        elif [ "mariadb-server" = "$i" ]; then
+                echo "el paquete: $i, no esta instalado"
+                echo "parametrizando $i"
+                password_mariadb=$(pwgen 25 1 --symbols)
+                echo "Instalando $i"
+                echo "mysql-server mysql-server/root_password password $password_mariadb" | sudo debconf-set-selections
+                echo "mysql-server mysql-server/root_password_again password $password_mariadb" | sudo debconf-set-selections
+                export DEBIAN_FRONTEND="noninteractive"
+                echo $password_mariadb 
+                sudo apt update -y
+                sudo apt install $i -y
+        else
+                echo "el paquete: $i, no esta instalado"
+                echo "Instalando $i"
+                sudo apt update -y
+                sudo apt install $i -y
+        fi
+done
+echo $password_mariadb 
