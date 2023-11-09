ed86fb12ed25bc24feefe5a6854182e08f6970ef
Javier Sepúlveda <javiersepulveda12@hotmail.com>
2023-11-09 18:29:43 -0500

    adding discord webhook

--- grupo7-deploy.sh
+++ grupo7-deploy.sh
@@ -10,7 +10,8 @@ fi
 password_mariadb=""
 repo="bootcamp-devops-2023"
 paquetes="git apache2 php pwgen mariadb-server"
-
+ip_ec2=$(curl ifconfig.me)
+DISCORD=""
 #cloning repo
 if [ "$(ls | grep $repo)" = "$repo" ]; then
         echo "el repo ya existe, nada para hacer"
@@ -75,13 +76,31 @@ for i in $paquetes; do
         fi
 done
 #testing status 200
-if [ "$(curl -s -o /dev/null -w "%{http_code}" http://localhost/index.php)" != "200" ]; then
+if [ "$(curl -s -o /dev/null -w "%{http_code}" http://$ip_ec2/app-295devops-travel/index.php)" != "200" ]; then
         echo "El sitio no esta ok"
-        exit 1
+        #exit 1
+        DEPLOYMENT_INFO="La página web http://$ip_ec2/app-295devops-travel/index.php no está en línea."
 else
-        echo "El sitio esta ok"
+        echo "El sitio esta ok: $(curl -s -o /dev/null -w "%{http_code}" http://$ip_ec2/app-295devops-travel/index.php)"
+        # Obtiene el nombre del repositorio
+        REPO_NAME=$(basename $(git rev-parse --show-toplevel))
+        # Obtiene la URL remota del repositorio
+        REPO_URL=$(git remote get-url origin)   REPO_NAME=$(basename $(git rev-parse --show-toplevel))
+        WEB_URL="http://$ip_ec2/app-295devops-travel/index.php"
+        echo $REPO_NAME
+        echo $REPO_URL
+        echo $WEB_URL
+        DEPLOYMENT_INFO2="Despliegue del repositorio $REPO_NAME: "
+        DEPLOYMENT_INFO="La página web $WEB_URL está en línea."
+        COMMIT="Commit: $(git rev-parse --short HEAD)"
+        AUTHOR="Autor: $(git log -1 --pretty=format:'%an')"
+        DESCRIPTION="Descripción: $(git log -1 --pretty=format:'%s')"
+        # Construye el mensaje
+        MESSAGE="$DEPLOYMENT_INFO2\n$DEPLOYMENT_INFO\n$COMMIT\n$AUTHOR\n$REPO_URL\n$DESCRIPTION"
+
+        # Envía el mensaje a Discord utilizando la API de Discord
+        curl -X POST -H "Content-Type: application/json" \
+        -d '{
+        "content": "'"${MESSAGE}"'"
+        }' "$DISCORD"
 fi
-
-
-
-
